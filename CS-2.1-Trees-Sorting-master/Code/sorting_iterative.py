94bf7629292d3bc071772cd2a6e89444d2e53c0e
Jarquevious <jarquevious@gmail.com>
2021-01-28 16:18:38 -0800

    all functions work

--- CS-2.1-Trees-Sorting-master/Code/sorting_iterative.py
+++ CS-2.1-Trees-Sorting-master/Code/sorting_iterative.py
@@ -6,6 +6,16 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    if len(items) == 0:
+        return True
+    
+    item_to_sort = items[0]
+    for i in range(1, len(items)):
+        if item_to_sort > items[i]:          
+            return False
+        item_to_sort = items[i]
+    return True
+
 
 
 def bubble_sort(items):
@@ -16,12 +26,11 @@ def bubble_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
      
-    isSort = False
+    isSort = False,
     while isSort == True:
         for i in range(len(items) - 1):
             if items[i] > items[i+1]:
                 items[i], items[i+1] = items[i+1], items[i]
-    print("Array is Bubble Sorted")
 
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
@@ -44,12 +53,6 @@ def selection_sort(items):
 
     return items
 
-
-
-
-
-
-
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
@@ -69,33 +72,33 @@ def insertion_sort(items):
     return items
             
 
-
+####Test Code for is_sort###
+sorted_items = [5,6,7,8,9]
+unsorted_items = [21, 6, 8, 7, 91, -1, 100, 34]
+print('Is_Sorted:')
+print(is_sorted(sorted_items))
+print(is_sorted(unsorted_items))   
+print('')
         
-        
-        
-        
-
-
-
 ####Test Code for bubble sort###
 # Array for items
-items = [56,34,24,14,10,4,6,3]
-#Call the bubble 
+items = [5,8,2,4,1,6,13,25,6,20]
+#Call the bubble function
 bubble_sort(items)
 # Print Statement to illustrate complete sorted array
+print("Bubble Sort:")
 print(items)
 
-
-
 ###Test Code for selection###
-items=[[56,34,24,14,10,4,6,3]]
+items=[5,8,2,4,1,6,13,25,6,20]
 print('')
+print('Selection Sort:')
 print(selection_sort(items))
 
 ###Test Code for insert###
 items=[376,2,6573,67,9,4,7,94,65,56745]
 print('')
-print('Array is Insertion Sorted')
+print('Insertion Sort:')
 insertion_sort(items)
 print(insertion_sort(items))
 
