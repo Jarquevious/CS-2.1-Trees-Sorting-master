74c31a417d02073a4679efe186f351ac7c325dfa
Jarquevious <jarquevious@gmail.com>
2021-01-30 15:40:57 -0800

    code cleaned up

--- CS-2.1-Trees-Sorting-master/Code/sorting_iterative.py
+++ CS-2.1-Trees-Sorting-master/Code/sorting_iterative.py
@@ -6,14 +6,20 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    # Use Boolean to check to see if size of array is 0. 
     if len(items) == 0:
         return True
     
     item_to_sort = items[0]
+    # Loop over each item from index 1
     for i in range(1, len(items)): #o(n)
-        if item_to_sort > items[i]:          
+        # Comparison operator to check values at each item
+        if item_to_sort > items[i]:
+            # Signify the output to user if false         
             return False
+        # Reassigning the items[i] for each item to for comparison
         item_to_sort = items[i]
+    # Signify the output to user if false
     return True
 
 
@@ -26,15 +32,15 @@ def bubble_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
     num_items = len(items)
-    # loop over items from 0 to length of items
+    # Loop over items from 0 to length of items
     for i in range(num_items-1):                        
-        # loop over items from 0 to i minus 1 for comparing
+        # Loop over items from 0 to i minus 1 for comparing
         for j in range(0, num_items-i-1):
-            # check if second to last item is greater than last item
+            # Check if second to last item is greater than last item
             if items[j] > items[j+1]:
-                # swapping
+                # Swap items 
                 (items[j], items[j+1]) = (items[j+1], items[j])
-                
+    # Return items at end of for loop           
     return items
 
 def selection_sort(items):
@@ -45,15 +51,21 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
-    # find lowest num in list compare to index 1; swap
+    
+    # Loop over each item from 0 to items[-1]
     for i in range(0, len(items) - 1):
+        # Assignn i as minimum value
         minimum_value = i
 
+        # Loop over items from right of i to end of list
         for j in range(i + 1, len(items)):
+            # Compare items[j] to the minimum value
             if items[j] < items[minimum_value]:
+                # If true, assign minimum value to j
                 minimum_value = j
-
+        # If item is not i
         if minimum_value != i:
+            # Swap items if true
             items[minimum_value], items[i] = items[i], items[minimum_value]
 
     return items
@@ -67,11 +79,13 @@ def insertion_sort(items):
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
     
-    # Search through the list starting at index 1.
+    # Loop over the list from index 1.
     for i in range(1, len(items)):
-
+        # Comparison operator 
         while items[i-1] > items[i] and i > 0:
+            # Swap items 
             items[i], items[i-1] = items[i-1], items[i]
+            # Continue looping over items
             i-=1
 
     return items
@@ -82,14 +96,13 @@ sorted_items = [5,6,7,8,9]
 unsorted_items = [21, 6, 8, 7, 91, -1, 100, 34]
 print('Is_Sorted:')
 print(is_sorted(sorted_items))
+print('Un_Sorted:')
 print(is_sorted(unsorted_items))   
-print('')
+
         
 ####Test Code for bubble sort###
-# Array for items
 items = [5,8,2,4,1,6,13,25,6,20]
-#Call the bubble function
-# Print Statement to illustrate complete sorted array
+print('')
 print("Bubble Sort:")
 print(bubble_sort(items))
 
@@ -104,6 +117,5 @@ print(selection_sort(items))
 items=[376,2,6573,67,9,4,7,94,65,56745]
 print('')
 print('Insertion Sort:')
-insertion_sort(items)
 print(insertion_sort(items))
 
