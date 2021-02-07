d7cc499876130b3c4f7b3252ce97fb229bfc0059
Jarquevious <jarquevious@gmail.com>
2021-02-07 14:57:06 -0800

    merge sort is working

--- CS-2.1-Trees-Sorting-master/Code/sorting_recursive.py
+++ CS-2.1-Trees-Sorting-master/Code/sorting_recursive.py
@@ -46,20 +46,69 @@ def merge(items1, items2):
     # Return newly sorted list as end result.
     return sorted_list
 
-items1 = [4,5,6,7,8,9]
-items2 = [23,24,25,26,27,28]
-new_list=merge(items1, items2)
-print(new_list)
 
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if list is so small it's already sorted (base case) 
-    # TODO: Split items list into approximately equal halves  
-    # TODO: Sort each half by recursively calling merge sort
-    # TODO: Merge sorted halves into one list in sorted order
+    # TODO: Check if list is so small it's already sorted (base case). 
+    if len(items) > 1:
+         # TODO: Split items list into approximately equal halves.
+        mid = len(items)//2
+        left_list = items[:mid]
+        right_list = items[mid:]
+
+        # TODO: Sort each half by recursively calling merge sort.
+        merge_sort(left_list)
+        merge_sort(right_list)
+
+        # TODO: Merge sorted halves into one list in sorted order
+    
+        # Create indexes for looping.
+        l = r = i = 0
+
+        # Create new variables equal to the length of both left and right list.
+        left_list_length = len(left_list)
+        right_list_length = len(right_list)
+    
+        # Loop through both list starting at index 0 until the list end simultaneously. 
+        while l < left_list_length and r < right_list_length:
+        
+            # Set conditional if index in left list is smaller than right list index
+            if left_list[l] < right_list[r]:
+           
+                # Set items[i] as left left list index
+                items[i] = left_list[l]
+                
+                # Move to the next index after each loop
+                l += 1
+        
+            # Set a conditional for items[i] if the condition above is not true
+            else:
+                items[i] = right_list[r]
+                # Move to next index and go through same process.
+                r += 1
+            i += 1
+
+        # Loop through left list from index 0 to the end of the list.
+        while l < left_list_length:
+            # Set items[i] as left list l
+            items[i] = left_list[l]
+            # Iterate to the next index and go through the same process. 
+            l += 1
+            i += 1
+    
+        # Loop though right list from index 0 until last index. 
+        while r < right_list_length:
+            # Set items[i] for each index represented by r.
+            items[i] = right_list[r]
+            # Iterate through each index.
+            r += 1
+            i += 1
+
+    # Return items in a sorted order.
+    return items
 
 
 
@@ -89,3 +138,17 @@ def quick_sort(items, low=None, high=None):
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+
+
+items1 = [4,5,6,7,8,9]
+items2 = [23,24,25,26,27,28]
+new_list=merge(items1, items2)
+print('Merge newly sorted list: ')
+print(new_list)
+
+
+items = [45,2,6,8,3,27,55,34,8,1,56,67,234]
+print('')
+print('Merge Sorted newly sorted list: ')
+new = merge_sort(items)
+print(new)
