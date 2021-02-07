b330c6c2b467ce043cb2a8e5c6e2123fef1853aa
Jarquevious <jarquevious@gmail.com>
2021-02-06 18:00:22 -0800

    Comments added to merge

--- CS-2.1-Trees-Sorting-master/Code/sorting_recursive.py
+++ CS-2.1-Trees-Sorting-master/Code/sorting_recursive.py
@@ -25,32 +25,26 @@ def merge(items1, items2):
     if not items1 and items2:
         return sorted_list + items2
 
+    # If both contain items
     if items1 and items2:
       
+      # Check if item1 at index 0 is lesser than or equal to 
+      # items2 index zero. If true add smallest item to sorted_list.
         if items1[0] <= items2[0]:
-             
              sorted_list.append(items1[0])
 
+            # Recursive call on the items1 index 1 until end of the list. 
              sorted_list = sorted_list + merge(items1[1:], items2)
 
+        # Add the smallest item from item items2 to sorted_list.
         if items1[0] > items2[0]:
-            
             sorted_list.append(items2[0])
 
+            # Items2 at index 0 is now apart of new list.
             sorted_list = sorted_list + merge(items1, items2[1:])
     
+    # Return newly sorted list as end result.
     return sorted_list
-
-
-
-
-        
-
-   
-   
- 
-     
-        
 
 items1 = [4,5,6,7,8,9]
 items2 = [23,24,25,26,27,28]
@@ -62,20 +56,14 @@ def merge_sort(items):
     sorting each recursively, and merging results into a list in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if list is so small it's already sorted (base case)
-   
-    # TODO: Split items list into approximately equal halves
-   
+    # TODO: Check if list is so small it's already sorted (base case) 
+    # TODO: Split items list into approximately equal halves  
     # TODO: Sort each half by recursively calling merge sort
     # TODO: Merge sorted halves into one list in sorted order
-#     if len(items) <= 1:
-#         return items
 
-#     left, right = merge_sort(items[:len(items)/2]), merge_sort(items[len(items)/2:])
-#     return merge(left, right)
 
-# items = [9,4,6,23,65,34,44]
-# print(merge_sort(items))
+
+
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
