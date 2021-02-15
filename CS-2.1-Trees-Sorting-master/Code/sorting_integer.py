2c91f0ff2552c363bd5f17ef3b8d6413f26ce336
Jarquevious <jarquevious@gmail.com>
2021-02-14 21:02:56 -0800

    both count and bucket works

--- CS-2.1-Trees-Sorting-master/Code/sorting_integer.py
+++ CS-2.1-Trees-Sorting-master/Code/sorting_integer.py
@@ -6,9 +6,25 @@ def counting_sort(numbers):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Find range of given numbers (minimum and maximum integer values)
+    minimum = min(numbers)
+    maximum = max(numbers)
+
     # TODO: Create list of counts with a slot for each number in input range
+    counts = [[x, 0] for x in set(numbers)]
+
     # TODO: Loop over given numbers and increment each number's count
+    for num in numbers:
+        for i in range(0, len(counts)):
+            if num == counts[i][0]:
+                counts[i][1] += 1
+
     # TODO: Loop over counts and append that many numbers into output list
+    output = []
+    for count in counts:
+        for _ in range(count[1]):
+            output.append(count[0])
+   
+    return output
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
@@ -42,25 +58,25 @@ def bucket_sort(numbers, num_buckets=10):
 
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     
+    for bucket in buckets:
+        for i in range(1, len(bucket)):
+            j = i - 1
+            num = bucket[i]
+            while j >= 0:
+                if bucket[i] < bucket[j]:
+                    bucket[j+1] = bucket[j]
+                    bucket[j] = num
+                    j -= 1
+                else:
+                    break
     # for bucket in buckets:
-    #     for i in range(1, len(bucket)):
-    #         j = i - 1
-    #         num = bucket[i]
-    #         while j >= 0:
-    #             if bucket[i] < bucket[j]:
-    #                 bucket[j+1] = bucket[j]
-    #                 bucket[j] = num
-    #                 j -= 1
-    #             else:
-    #                 break
-
-    for i in range(1, len(buckets)):
-        # Comparison operator 
-        while buckets[i-1] > buckets[i] and i > 0:
-            # Swap items 
-            buckets[i], buckets[i-1] = buckets[i-1], buckets[i]
-            # Continue looping over items
-            i-=1
+    #     for i in range(1, len(buckets)):
+    #         # Comparison operator 
+    #         while buckets[i-1] > buckets[i] and i > 0:
+    #             # Swap items 
+    #             buckets[i], buckets[i-1] = buckets[i-1], buckets[i]
+    #             # Continue looping over items
+    #             i-=1
 
     return buckets
 
@@ -74,5 +90,7 @@ def bucket_sort(numbers, num_buckets=10):
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
-numbers=[10,3,6,2,8]
+numbers=[10,3,6,2,8, 100, 4,56,2345,576,3,6,3472,7,3,2,78,93,76,23,456,7,3,56,23, 29,57]
 print(bucket_sort(numbers, num_buckets=10))
+print('')
+print(counting_sort(numbers))
